a15c3aab238ad7763390983929c4559ff74e6ba9
Rutvik Parekh <72230078+rutvik1942@users.noreply.github.com>
2024-08-12 13:01:20 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,16 +1,7 @@
 const mongoose = require('mongoose');
+const url = 'mongodb+srv://chat_app_admin:Gj05pt9886@cluster0.ch4ax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-//const url = 'mongodb+srv://<chat_app_admin>:<Gj05pt9886>@cluster0.mongodb.net/<chat_app_admin>?retryWrites=true&w=majority';
-
-const username = encodeURIComponent("<username>");
-const password = encodeURIComponent("<password>");
-const cluster = "<Cluster0>";
-const authSource = "<authSource>";
-const authMechanism = "<authMechanism>";
-let uri =
-  `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
-
-mongoose.connect(uri, {
+mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true
   }).then(() => console.log('connected to DB')).catch((e)=> console.log('Error', e))
