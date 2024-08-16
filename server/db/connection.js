8a8143fda8353c6a44e1154a82dc4e66c02fac9f
Rutvik Parekh <72230078+rutvik1942@users.noreply.github.com>
2024-08-16 10:01:04 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,4 +1,3 @@
 const mongoose = require('mongoose');
 const url = 'mongodb+srv://chat_app_admin:Gj05pt9886@cluster0.ch4ax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
 mongoose.connect(url);
