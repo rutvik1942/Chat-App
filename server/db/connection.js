eb77e02b1426728f83c7bd9cf1980d276b241ef8
Rutvik Parekh <72230078+rutvik1942@users.noreply.github.com>
2024-08-16 10:00:37 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
 const url = 'mongodb+srv://chat_app_admin:Gj05pt9886@cluster0.ch4ax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-  }).then(() => console.log('connected to DB')).catch((e)=> console.log('Error', e))
+mongoose.connect(url);
